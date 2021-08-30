25b56acd8a3699f1c85c954d00b9954833b4c03e
Cassiano Thurow <cassiano@bemobile.tech>
2021-08-30 16:20:20 -0300

    ✨ Payments stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,17 +1,55 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from 'next-auth'
+import { getSession } from 'next-auth/client'
+import { fauna } from "../../services/fauna";
+import { query as q } from 'faunadb'
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  }
+  data: {
+    stripe_costumer_id: string;
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req })
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email
-    })
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let costumerId = user.data.stripe_costumer_id
+
+    if(!costumerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email
+      })
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_costumer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+      costumerId = stripeCustomer.id
+    }
+
+
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: costumerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
@@ -19,8 +57,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       ],
       mode: 'subscription',
       allow_promotion_codes: true,
-      success_url: process.env.STIPE_SUCCESS_URL,
-      cancel_url : process.env.STIPE_CANCEL_URL
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url : process.env.STRIPE_CANCEL_URL
     })
     return res.status(200).json({sessionId: stripeCheckoutSession.id});
   } else {
