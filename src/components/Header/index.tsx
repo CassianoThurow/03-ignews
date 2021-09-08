e4fd4db2f9dbf4c202a6d118687c0e575ea4693c
Cassiano Thurow <cassiano.thurow@gmail.com>
2021-09-08 17:10:38 -0300

    ✨ active link header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { SignInButton } from '../SignInButton'
 
 import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink'
 
 export function Header() {
+  const { asPath } = useRouter()
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
+                  <ActiveLink activeClassName={styles.active} href="/">
+                    <a>Home</a>
+                  </ActiveLink>
+                  <ActiveLink activeClassName={styles.active} href="/posts">
                     <a >Posts</a>
+                  </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>
-            
+
         </header>
     )
 }
